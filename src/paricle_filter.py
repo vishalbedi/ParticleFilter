5f99145497e8cd838503b566101b1945020b2190
Vishal <vishal.b1989@gmail.com>
2018-05-01 15:34:19 -0400

    added clustering

--- src/paricle_filter.py
+++ src/paricle_filter.py
@@ -2,6 +2,8 @@ from math import sqrt, pi, e
 import particle
 import utils
 import numpy as np
+import transformations
+import random
 
 
 class ParticleFilter:
@@ -16,6 +18,10 @@ class ParticleFilter:
         self.laser_max = laser_max
         self.pixels_per_meter = pixels_per_meter
         self.particle_set = [self.generate_random_particle() for _ in range(self.PARTICLE_COUNT)]
+        self.isLocalized = False
+        self.isClustering = False
+        self.CLUSTER_SIZE = 15
+        self.NEIGHBOR_THRESHOLD = 0.2
 
     def generate_random_particle(self):
         """
@@ -89,9 +95,65 @@ class ParticleFilter:
                 p.weight = 1.0
 
     def filter(self, sensor_msg, twist):
+        # don't update if clustering
+        if self.isClustering:
+            return
         self.sensor_scan(sensor_msg)
         self.resample()
         self.motion_sense(twist)
 
     def clustering(self):
-        pass
+        """
+        Perform K means clustering to get the centroid of the particles
+        do not perform any update while clustering
+        :return:
+        """
+        while True:
+            self.isClustering = True
+            particles = self.particle_set
+            particles = sorted(particles, key=lambda k: (k['x'], k['y'], k['theta']))
+            visited = set()
+            clusters = {}
+            cluster_count = 0
+            # create clusters
+            for i in range(self.PARTICLE_COUNT):
+                if i not in visited:
+                    queue = [i]
+                    cluster_count +=1
+                    clusters[cluster_count] = []
+                    while len(queue):
+                        current_particle_index = queue.pop()
+                        current_particle = particles[current_particle_index]
+                        clusters[cluster_count].append(current_particle)
+                        end_particle_index = current_particle + self.CLUSTER_SIZE
+                        if end_particle_index > self.PARTICLE_COUNT:
+                            end_particle_index = self.PARTICLE_COUNT
+                        cluster_distances = []
+                        for neighbour_index in range(current_particle_index, end_particle_index):
+                            if neighbour_index not in visited:
+                                neighbour_particle = particles[neighbour_index]
+                                cluster_distance = (transformations.dist((current_particle.x, current_particle.y), (neighbour_particle.x, neighbour_particle.y)), neighbour_index)
+                                cluster_distances.append(cluster_distance)
+                        cluster_distances.sort(lambda a, b: cmp(a[0], b[0]))
+                        for distance, index in cluster_distances:
+                            if distance < self.NEIGHBOR_THRESHOLD:
+                                visited.add(neighbour_index)
+                                queue.append(neighbour_index)
+                            else:
+                                break
+            biggest_cluster_size = 0
+            for cluster in clusters.values():
+                if len(cluster) > biggest_cluster_size:
+                    biggest_cluster_size = len(cluster)
+                    biggest_cluster = cluster
+            cluster_coordinates = [(p.x, p.y) for p in biggest_cluster]
+            cluster_coordinates = np.array(cluster_coordinates)
+            centroid = np.mean(cluster_coordinates, axis=0)
+            variance = np.var(cluster_coordinates, axis=0)
+            self.centroid = (centroid[0],centroid[1], biggest_cluster[random.randint(0,biggest_cluster_size-1)].theta)
+
+            if biggest_cluster_size > self.PARTICLE_COUNT * .75:
+                self.isLocalized = True
+            else:
+                self.isLocalized = False
+            self.isClustering = False
