7bfd5cbdbbe8ac13875ebb3f17fbbae53bee9d8a
Vishal <vishal.b1989@gmail.com>
2018-05-01 22:50:27 -0400

    error fix

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,10 @@ FOCUS_POINT = [(8, -0.5), (-12.0, 12.0), (-18.4, -8.9), (10.8, 12.7), (-54.5, 7.
 
 
 def generate_random_pose():
-    index = random.uniform(0,5)
+    index = random.randint(0, 5)
     deveation = 10
     x = random.gauss(FOCUS_POINT[index], deveation)
-    y = random.uniform(FOCUS_POINT[index], deveation)
+    y = random.gauss(FOCUS_POINT[index], deveation)
     theta = random.uniform(0, 2 * pi)
     return x, y, theta
 
