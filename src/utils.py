a08d4796e570b956e832e7a8cb884716956ec91c
Vishal Girish Bedi <vgb8777@cs.rit.edu>
2018-05-01 22:40:57 -0400

    changed order of particle creation

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,10 @@ import random
 from math import pi
 
 
-def generate_random_pose(MAP_WIDTH, MAP_HEIGHT):
-    x = random.uniform(-MAP_WIDTH/2, MAP_WIDTH/2)
-    y = random.uniform(-MAP_HEIGHT/2, MAP_HEIGHT/2)
+def generate_random_pose(focus_x, focus_y):
+    deveation = 20
+    x = random.gauss(focus_x, deveation)
+    y = random.uniform(focus_y, deveation)
     theta = random.uniform(0, 2 * pi)
     return x, y, theta
 
